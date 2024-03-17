ad802e450600cddb72da550c1239dc5358f4a287
Christopher Hamilton <chrish001@live.com.au>
2024-03-17 14:52:07 +1030

    feat: add progress bars to goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,20 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar'; 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-	RouterOutlet,
-	MatDatepickerModule,
-	MatFormFieldModule,
-	MatNativeDateModule,
-	MatInputModule,
-  FormsModule,
-  ReactiveFormsModule,
-  MatButtonModule,
+    RouterOutlet,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatProgressBarModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -27,6 +28,10 @@ export class AppComponent {
   title = 'planning-ui';
 
   date = new FormControl(new Date());
+
+  goal1 = 0;
+  goal2 = 0;
+  goal3 = 0;
 
   field1 = 0;
   field2 = 0;
