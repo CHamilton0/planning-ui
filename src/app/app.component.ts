2015144943489296d5f8f2380629a5cb69174feb
Christopher Hamilton <chrish001@live.com.au>
2024-03-17 14:30:45 +1030

    feat: add date selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+	RouterOutlet,
+	MatDatepickerModule,
+	MatFormFieldModule,
+	MatNativeDateModule,
+	MatInputModule,
+  FormsModule,
+  ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'planning-ui';
+
+  date = new FormControl(new Date());
 }
+
