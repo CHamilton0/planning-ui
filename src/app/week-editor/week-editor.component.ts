2ed9cc2ee93ddc9767767bb74b94095076fd5efc
Christopher Hamilton <chrish001@live.com.au>
2024-03-25 19:33:22 +1030

    feat: add week editing

--- src/app/week-editor/week-editor.component.ts
+++ src/app/week-editor/week-editor.component.ts
@@ -1,5 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -13,10 +21,46 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
+    CommonModule,
   ],
   templateUrl: './week-editor.component.html',
-  styleUrl: './week-editor.component.scss'
+  styleUrl: './week-editor.component.scss',
 })
 export class WeekEditorComponent {
-  
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      formGroups: this.fb.array([]),
+    });
+  }
+
+  ngOnInit() {
+    this.addFormGroup();
+  }
+
+  get formGroups(): FormArray {
+    return this.myForm.get('formGroups') as FormArray;
+  }
+
+  addFormGroup(): void {
+    const formGroup = this.fb.group({
+      // Define your form group structure here
+      name: ['', Validators.required],
+      minGoal: [0, [Validators.required, Validators.min(1)]],
+      maxGoal: undefined,
+    });
+
+    formGroup.valueChanges.subscribe((value) => {
+      if (formGroup.valid) {
+        console.log(value);
+      }
+    });
+
+    this.formGroups.push(formGroup);
+  }
+
+  deleteFormGroup(index: number): void {
+    this.formGroups.controls.splice(index, 1);
+  }
 }
