b68b29ef5bd3434e9b9d45845f651df44e5d394e
Christopher Hamilton <chrish001@live.com.au>
2024-04-16 19:15:22 +0930

    feat: style summary based on how close hours are to goals

--- src/app/interfaces/summary.ts
+++ src/app/interfaces/summary.ts
@@ -1,10 +1,11 @@
 import { Goal } from "./goal";
 
 export interface Summary extends Goal {
-    hoursDone: number
+    hoursDone: number;
 }
 
 export interface SummaryDisplay extends Summary {
     hoursDoneStyle?: string;
     goalStyle?: string;
+    percentageDifference?: number;
 }
